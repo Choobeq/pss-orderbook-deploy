d836e3a0e5e19aa083a2f098c907a7c426ca7b13
Tomasz Wisniewski <133998946+Choobeq@users.noreply.github.com>
2024-02-28 14:05:22 +0000

    Update main.py
    Updating endpoints

--- src/currencyAPI/app/main.py
+++ src/currencyAPI/app/main.py
@@ -42,7 +42,6 @@ async def convert_amount(from_currency: str, to_currency: str, amount: float) ->
         "amount": amount,
         "converted_amount": converted_amount,
     }
-  
 
 @app.get("/check_password_strength")
 async def check_password_strength(password: str) -> dict:
@@ -109,13 +108,14 @@ async def get_usd_rate() -> dict:
 @app.get("/convert_crypto")
 async def convert_crypto(from_crypto: str, to_currency: str, amount: float) -> dict:
     # """
-    # Coded by: Bette Beaument
+    # Coded by: Tomasz Wisniewski & Bette Beament
     # This endpoint allows you to convert crypto into any currency.
+    # This endpoint includes a function to display an error when an invalid crypto currency has been entered.
     # """
     try:
-        check_result = await check_crypto(from_crypto)
+        check_result = await check_all_currencies(from_crypto)
         if "error_message" in check_result:
-            raise HTTPException(status_code=404, detail=check_result["error_message"])
+            raise HTTPException(status_code=400, detail=check_result["error_message"])
  
         crypto_rate = await get_usd_rate()
         ex_rate = await exchange_rate(to_currency, "USD")
@@ -134,12 +134,6 @@ async def convert_crypto(from_crypto: str, to_currency: str, amount: float) -> d
         raise
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
- 
-#@CODE : ADD ENDPOINT TO UPDATE PRICE OF ASSET IN ORDERBOOK DB
-#The code below starts you off using SQLAlchemy ORM
-#Dependencies should already be installed from your requirements.txt file
-#Using the ORM instead of raw SQL is safer and less coupled: it is best practice!
  
 @app.get("/update_orderbookdb_asset_price")
 async def update_orderbookdb_asset_price(symbol: str, new_price: float) -> dict:
@@ -309,20 +303,43 @@ async def compare_currencies(currency_1: str, currency_2: str) -> dict:
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-async def get_historical_data(currency: str) -> dict:
+async def get_historical_data(from_currency: str, to_currency: str) -> dict:
     # """
     # Coded by: Tomasz Wisniewski
     # This function downloads historical rate against USD for required currency.
     # """
-    response = requests.get(f"{FREECURRENCYAPI}&base_currency=USD&currencies={currency}")
+    response = requests.get(f"{FREECURRENCYAPI}&base_currency={from_currency}&currencies={to_currency}")
     if response.status_code == 200:
         data = response.json()
         return data["data"]
     else:
         raise HTTPException(status_code=400, detail="Failed to fetch USD the historical data")
- 
+
+async def new_price_vs_old_price(new_price: float, old_price: float, from_currency: str, to_currency: str) -> dict:
+    # """
+    # Coded by: Tomasz Wisniewski
+    # This function compares old_price vs new_price and returns the final result.
+    # It was created as I need assign prices seperately for currency and crypto
+    # """
+    global percentage
+    percentage = 0
+    if old_price > new_price:
+        percentage = round((old_price - new_price) / old_price, 3)
+        return {"message": f"Today {from_currency.upper()} is worth {round(old_price-new_price,4)} more than yesterday against {to_currency.upper()}.",
+                "price": f"Todaya's price for {from_currency.upper()} = {1*new_price} {to_currency.upper()}",
+                "percentage": f"Percentage change = {percentage*100}%"}
+    if old_price < new_price:
+        percentage = round((new_price - old_price) / new_price, 3)
+        return {"message": f"Today {from_currency.upper()} is worth {round(new_price-old_price,4)} less than yesterday against {to_currency.upper()}",
+                "price": f"Todaya's price for {from_currency.upper()} = {1*new_price} {to_currency.upper()}",
+                "percentage": f"Percentage change = -{percentage*100}%"}
+    else:
+        return {"message": f"{from_currency.upper()} price hasn't change since yesterday against {to_currency.upper()}.",
+                "price": f"Todaya's price for {from_currency.upper()} = {1*new_price} {to_currency.upper()}",
+                "percentage": f"Percentage change = {percentage*100}"}
+
 @app.get("/check_yesterdays_price")
-async def check_yesterdays_price(currency: str) -> dict:
+async def check_yesterdays_price(from_currency: str, to_currency: str) -> dict:
     # """
     # Coded by: Tomasz Wisniewski and Bette Beament
     # This endpoint will compare current price with the price from previous day against USD
@@ -332,23 +349,32 @@ async def check_yesterdays_price(currency: str) -> dict:
     y_day.append(today_d - timedelta(days = 1))
     yesterday = y_day[0]
     try:
-        old_data = await get_historical_data(currency)
-        old_price = old_data[str(yesterday)][currency.upper()]
-        #print(old_data)
-        new_data = await exchange_rate("USD", currency)
-        new_price = new_data['exchange_rate']
- 
-        if old_price > new_price:
-            return {"message": f"{currency} is worth more than yesterday (in USD)."}
-        if old_price < new_price:
-            return {"message": f"{currency} is worth less than yesterday (in USD)."}
+        if len(from_currency) > 7 or len(to_currency) > 7:
+            raise HTTPException(status_code=404, detail="This cannot be currency or crypto. Max 7 letters allowed! If you need to find out the ID use /'Check Crypto' or /'Available currencies' endpoint.")
+        check_1st_currency = await available_currencies()
+        if from_currency.upper() not in check_1st_currency['available_currencies']:
+            raise HTTPException(status_code=404, detail=(f"The ID '{from_currency.upper()}' is NOT on the market."))
         else:
-            return {"message": f"{currency} price hasn't change since yesterday (in USD)."}
-       
+            check_2nd_currency = await available_currencies()
+            if to_currency.upper() not in check_2nd_currency['available_currencies']:
+                raise HTTPException(status_code=404, detail=(f"The ID '{to_currency.upper()}' is NOT on the market."))
+        #check_crypto = await available_crypto(from_currency)
+        check_result = await check_all_currencies(from_currency)
+        print(len(from_currency))
+        if "error_message" in check_result:
+            raise HTTPException(status_code=404, detail=check_result["error_message"])
+        old_data = await get_historical_data(from_currency.upper(), to_currency.upper())
+        old_price = round(old_data[str(yesterday)][to_currency.upper()],3)
+        new_data = await exchange_rate(from_currency, to_currency)
+        new_price = round(new_data['exchange_rate'],3)
+        #
+        verification = await new_price_vs_old_price(new_price, old_price, from_currency, to_currency)
+        return verification
+    except HTTPException:
+        raise
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="An error occurred while receiving data")
-
 
 @app.post("/track-portfolio")
 async def track_portfolio(holdings: dict):
